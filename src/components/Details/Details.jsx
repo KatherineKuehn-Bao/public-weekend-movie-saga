424351978d5dafada4a0ca9c596420ffd4074036
Katherine Daugherty <katherinedaugherty23@gmail.com>
2021-11-06 17:16:24 -0500

    save

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -9,39 +9,40 @@ function Details() {
     const genres = useSelector(store => store.genres);
     const movies = useSelector((store) => store.movie);
     const selectedId = useSelector(store => store.selectedId)
+    // const selectedMovie = useSelector((store) => store.)
 
-    let selectedMovie = movies.filter(movie => movie.id === selectedId)
+    // let selectedMovie = movies.filter(movie => movie.id === selectedId)
 
-    useEffect(() => {
-    })
+    // useEffect(() => {
+    // })
 
-    console.log(selectedMovie);
-    console.log(genres);
-    console.log(movies);
-
-    return (
-        <div>
-            {genres.map(genre => genre.name).join(', ')}
-        </div>
-    );
-
-
+    // console.log(selectedMovie);
+    // console.log(genres);
+    // console.log(movies);
 
     // return (
-    //     <section>
-    //         <h1> Selected Movie </h1>
-    //         {
-    //             selectedMovie.title ? (
-    //                 <>
-    //                     <h2> {selectedMovie.title} </h2>
-    //                     <img src={movies.poster} alt={movies.title} />
-    //                 </>
-    //             ) : (
-    //                 <p> No Movie Selected </p>
-    //             )
-    //         }
-    //     </section>
+    //     <div>
+    //         {genres.map(genre => genre.name).join(', ')}
+    //     </div>
     // );
+
+
+
+    return (
+        <section>
+            <h1> Selected Movie </h1>
+            {
+                selectedMovie.title ? (
+                    <>
+                        <h2> {selectedMovie.title} </h2>
+                        <img src={movies.poster} alt={movies.title} />
+                    </>
+                ) : (
+                    <p> No Movie Selected </p>
+                )
+            }
+        </section>
+    );
 }
 
 export default Details;
