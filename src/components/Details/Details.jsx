8e146a2aae5b8dc6a5bb8bfd97b964e25dc3c2bf
Katherine Daugherty <katherinedaugherty23@gmail.com>
2021-11-06 16:59:39 -0500

    save

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 //NEED MORE INFORMATION FROM THE DATABASE..... 
 // import { useEffect } from "react";
@@ -5,24 +6,42 @@ import { useSelector } from "react-redux";
 //Show the selected movie data from Redux
 function Details() {
     //data from redux
-    const selectedMovie = useSelector((store) => store.selectedMovie);
-    const movie = useSelector((store) => store.movie);
+    const genres = useSelector(store => store.genres);
+    const movies = useSelector((store) => store.movie);
+    const selectedId = useSelector(store => store.selectedId)
+
+    let selectedMovie = movies.filter(movie => movie.id === selectedId)
+
+    useEffect(() => {
+    })
+
+    console.log(selectedMovie);
+    console.log(genres);
+    console.log(movies);
 
     return (
-        <section>
-            <h1> Selected Movie </h1>
-            {
-                selectedMovie.title ? (
-                    <>
-                    <h2> {selectedMovie.title} </h2>
-                    <img src={movie.poster} alt={movie.title} />
-                    </>
-                ) : (
-                    <p> No Movie Selected </p>
-                )
-            }
-        </section>
+        <div>
+            {genres.map(genre => genre.name).join(', ')}
+        </div>
     );
+
+
+
+    // return (
+    //     <section>
+    //         <h1> Selected Movie </h1>
+    //         {
+    //             selectedMovie.title ? (
+    //                 <>
+    //                     <h2> {selectedMovie.title} </h2>
+    //                     <img src={movies.poster} alt={movies.title} />
+    //                 </>
+    //             ) : (
+    //                 <p> No Movie Selected </p>
+    //             )
+    //         }
+    //     </section>
+    // );
 }
 
 export default Details;
